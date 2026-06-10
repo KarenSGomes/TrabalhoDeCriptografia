2d0e86e429bf26f8e3e76c96331c8f3bfced34a9
Karen Gomes <karen.gomes@alunos.ufersa.edu.br>
2026-06-10 14:45:59 -0300

    feat: initial commit with Spring Boot setup

---
+++ src/main/java/com/cripto/chatCriptografado/ChatCriptografadoApplication.java
@@ -0,0 +1,13 @@
+package com.cripto.chatCriptografado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatCriptografadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatCriptografadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cripto/chatCriptografado/ChatCriptografadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cripto.chatCriptografado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatCriptografadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
